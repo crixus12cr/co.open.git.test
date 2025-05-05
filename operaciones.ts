87c0db4c49ed6d7ae5d75f4938754f4d61b3586e
crixus12cr <cristian2020til@gmail.com>
2025-05-05 06:53:46 -0500

    se agrego el log al inicio, cambios en la funcion de suma por opAdd mas un nuevo parametro y una nueva funcion opLess

--- operaciones.ts
+++ operaciones.ts
@@ -1,3 +1,9 @@
-function sumar(a: number, b: number): number {
-    return a + b;
+console.log('Operaciones Matemáticas');
+
+function opAdd(a: number, b: number, c: number): number {
+    return a + b + c;
+}
+
+function opLess(a: number, b: number): number {
+    return a - b;
 }
